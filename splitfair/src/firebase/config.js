17ee243c51a4f8ad98cd889f7160e4c7a217bed0
je1ii <167274389+je1ii@users.noreply.github.com>
2025-08-02 01:27:15 +0800

    update config

--- splitfair/src/firebase/config.js
+++ splitfair/src/firebase/config.js
@@ -3,12 +3,12 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA-RsTz2R9R8tRWrTJN2W6FxexhcrdH3TU",
-  authDomain: "splitfair-25045.firebaseapp.com",
-  projectId: "splitfair-25045",
-  storageBucket: "splitfair-25045.firebasestorage.app",
-  messagingSenderId: "781160293886",
-  appId: "1:781160293886:web:edf9c2b3df95f9fa500caf"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 const app = initializeApp(firebaseConfig);
